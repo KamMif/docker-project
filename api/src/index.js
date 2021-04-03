c29659fdb4d337dd137cd04c1f865174ecf31087
KamMif <kammif1990@gmail.com>
2021-04-03 18:59:47 +0500

    add volume database and depends on api services

--- api/src/index.js
+++ api/src/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const { port } = require("./configuration");
 const { connectDB } = require("./helpers/db");
+const Mongoose = require('mongoose');
 const app = express();
+
+const postSchema = new Mongoose.Schema({
+    name: String,
+});
+const Post = Mongoose.model('Post', postSchema);
 
 const startServer = () => {
     app.listen(port, () => {
         console.log(`Server start on port: ${port}`);
+        const silence = new Post({name: 'Silence'});
+        console.log({name: silence});
     })
 }
 
