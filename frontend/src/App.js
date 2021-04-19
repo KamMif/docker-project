a06d0285412311a19554b36ad742a73fed9dd21a
KamMif <kammif1990@gmail.com>
2021-04-19 20:42:52 +0300

    last module in course

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const makeApiRequest = () => {
+    axios.get('/api//testWithCurrentUser').then(({data}) => {
+        console.log({data});
+    })
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload111.
+          Edit <code>src/App.js</code> and save to reload dev.
         </p>
         <a
           className="App-link"
@@ -18,6 +24,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <button onClick={makeApiRequest}>make requies</button>
     </div>
   );
 }
